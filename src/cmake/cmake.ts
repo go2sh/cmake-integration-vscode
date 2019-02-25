ae8a25f456c4b8e72d1fbc62428bf21e1893d5b8
Christoph Seitz <christoph.seitz@posteo.de>
2019-02-25 23:07:04 +0100

    Add project context to CMake.

--- src/cmake/cmake.ts
+++ src/cmake/cmake.ts
@@ -12,6 +12,18 @@ const unlink = util.promisify(fs.unlink);
 const mkdir = util.promisify(fs.mkdir);
 const rmdir = util.promisify(fs.rmdir);
 
+class ProjectContext {
+  currentTargetName: string = "";
+}
+
+interface ProjectContextMap {
+  [key: string]: ProjectContext;
+}
+
+class ClientContext {
+  currentProjectName: string = "";
+  currentConfiguration: string = "Debug";
+  projectContexts: ProjectContextMap = {};
 }
 
 abstract class CMake implements vscode.Disposable {
@@ -188,6 +200,70 @@ abstract class CMake implements vscode.Disposable {
   protected mayShowConsole() {
     if (vscode.workspace.getConfiguration("cmake").get("showConsoleAutomatically", true)) {
       this.console.show();
+    }
+  }
+
+  /*
+   * Context handling
+   *
+   */
+  protected clientContext: ClientContext;
+
+  protected get currentProjectContext(): ProjectContext | undefined {
+    if (this.project) {
+      let projectContext: ProjectContext;
+      if (!this.clientContext.projectContexts.hasOwnProperty(this.project.name)) {
+        projectContext = new ProjectContext();
+        this.clientContext.projectContexts[this.project.name] = projectContext;
+      } else {
+        projectContext = this.clientContext.projectContexts[this.project.name];
+      }
+      return projectContext;
+    }
+    return undefined;
+  }
+
+  protected updateContext() {
+    if (this.project) {
+      this.clientContext.currentProjectName = this.project.name;
+
+      if (this.target) {
+        this.currentProjectContext!.currentTargetName = this.target.name;
+      } else {
+        this.currentProjectContext!.currentTargetName = "";
+      }
+    } else {
+      this.clientContext.currentProjectName = "";
+    }
+    this.extensionContext.workspaceState.update(this.name + "-context", this.clientContext);
+  }
+
+  protected selectContext() {
+    this._projectTargets.clear();
+    this._targetProject.clear();
+
+    this._projects.forEach((project) => {
+      this._targets.splice(this._targets.length, 0, ...project.targets);
+      this._projectTargets.set(project, project.targets);
+      project.targets.forEach((target) => this._targetProject.set(target, project));
+    });
+
+    if (this._projects.length > 0) {
+      this._project = this._projects.find((value) => value.name === this.clientContext.currentProjectName) || this._projects[0];
+      this.clientContext.currentProjectName = this._project.name;
+
+      let context = this.currentProjectContext!;
+      let targets = this.projectTargets;
+      if (targets && targets.length > 0) {
+        let target = targets.find((value) => context.currentTargetName === value.name) || targets[0];
+        this._target = target;
+        context.currentTargetName = target.name;
+      } else {
+        this._target = undefined;
+      }
+    } else {
+      this._project = undefined;
+      this._target = undefined;
     }
   }
 }
