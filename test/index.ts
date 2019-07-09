baad2fffde64bcd0bed7758607f4571f70f4e631
Christoph Seitz <christoph.seitz@posteo.de>
2019-07-09 22:02:14 +0200

    Upgrade tests to new packages.

--- test/index.ts
+++ test/index.ts
@@ -17,11 +17,40 @@
 /*
  * Testrunner
  */
-import * as testRunner from 'vscode/lib/testrunner';
+import * as path from 'path';
+import Mocha from 'mocha';
+import glob from 'glob';
 
-testRunner.configure({
-    ui: 'tdd',
-    useColors: true
-});
+export function run(): Promise<void> {
+  // Create the mocha test
+  const mocha = new Mocha({
+    ui: 'tdd'
+  });
+  mocha.useColors(true);
 
-module.exports = testRunner;
+  const testsRoot = path.resolve(__dirname, '..');
+
+  return new Promise((c, e) => {
+    glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
+      if (err) {
+        return e(err);
+      }
+
+      // Add files to the test suite
+      files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+
+      try {
+        // Run the mocha test
+        mocha.run(failures => {
+          if (failures > 0) {
+            e(new Error(`${failures} tests failed.`));
+          } else {
+            c();
+          }
+        });
+      } catch (err) {
+        e(err);
+      }
+    });
+  });
+}
