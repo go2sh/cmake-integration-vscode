4b65b1a0f7217d549d82b1d4fdd8a6e168036a76
Christoph Seitz <christoph.seitz@posteo.de>
2019-10-31 22:45:31 +0100

    Rewrite quick pick code.

--- src/helpers/quickPick.ts
+++ src/helpers/quickPick.ts
@@ -12,202 +12,176 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ */
 /*
  * Handling all quick pick action for clients, projects and targets.
  */
 import * as vscode from "vscode";
-import * as model from '../cmake/model';
+import * as model from "../cmake/model";
 import { CMakeClient } from "../cmake/cmake";
 import { CMakeConfiguration } from "../cmake/config";
 
+async function pick<T extends vscode.QuickPickItem>(
+  items: T[],
+  activeItem: T | undefined
+) {
+  let pick = vscode.window.createQuickPick<T>();
+  pick.items = items;
+  pick.activeItems = activeItem ? [activeItem] : [];
+  pick.show();
+
+  return new Promise<T | undefined>((resolve) => {
+    pick.onDidChangeSelection((items) => {
+      const result = items[0];
+      if (result) {
+        resolve(result);
+        pick.hide();
+      }
+    });
+    pick.onDidChangeValue((value) => {
+      if (
+        activeItem &&
+        !value &&
+        (pick.activeItems.length !== 1 || pick.activeItems[0] !== activeItem)
+      ) {
+        pick.activeItems = [activeItem];
+      }
+    });
+    pick.onDidAccept(() => {
+      const result = pick.activeItems[0];
+      if (result) {
+        resolve(result);
+        pick.hide();
+      }
+    });
+    pick.onDidHide(() => {
+      resolve(undefined);
+      pick.dispose();
+    });
+  });
+}
 interface CMakeItem extends vscode.QuickPickItem {
-    client: CMakeClient;
+  client: CMakeClient;
 }
 
-async function pickClient(clients: CMakeClient[]): Promise<CMakeClient | undefined> {
+async function pickClient(
+  clients: CMakeClient[],
+  activeClient?: CMakeClient
+): Promise<CMakeClient | undefined> {
+  let items = clients.map((value) => {
+    return {
+      client: value,
+      label: value.name,
+      description: value.sourceUri.fsPath
+    } as CMakeItem;
+  });
 
-    let clientPick = vscode.window.createQuickPick<CMakeItem>();
-    clientPick.items = clients.map((value) => {
-        return {
-            client: value,
-            label: value.name,
-            description: value.sourceUri.fsPath
-        } as CMakeItem;
-    });
-    clientPick.show();
-
-    return new Promise<CMakeClient | undefined>((resolve) => {
-        let activeItem: CMakeItem;
-        clientPick.onDidChangeSelection((e) => {
-            const result = clientPick.activeItems[0];
-            if (result) {
-                resolve(result.client);
-                clientPick.hide();
-            }
-        });
-        clientPick.onDidChangeValue(value => {
-            if (activeItem && !value && (clientPick.activeItems.length !== 1 || clientPick.activeItems[0] !== activeItem)) {
-                clientPick.activeItems = [activeItem];
-            }
-        });
-        clientPick.onDidAccept((e) => {
-            const result = clientPick.activeItems[0];
-            if (result) {
-                resolve(result.client);
-                clientPick.hide();
-            }
-        });
-        clientPick.onDidHide((e) => {
-            resolve(undefined);
-            clientPick.dispose();
-        });
-    });
+  let result = await pick(
+    items,
+    items.find((item) => item.client === activeClient)
+  );
+  if (result) {
+    return result.client;
+  } else {
+    return undefined;
+  }
 }
 
 interface ProjectContext {
-    client: CMakeClient;
-    project: model.Project;
+  client: CMakeClient;
+  project: model.Project;
 }
 
 interface ProjectContextItem extends vscode.QuickPickItem {
-    context: ProjectContext;
+  context: ProjectContext;
 }
 
+async function pickProject(
+  projects: ProjectContext[],
+  activeItem?: ProjectContext
+): Promise<ProjectContext | undefined> {
+  let items = projects.map((value) => {
+    return {
+      context: value,
+      label: value.project.name,
+      description: value.client.name
+    } as ProjectContextItem;
+  });
 
-async function pickProject(projects: ProjectContext[]): Promise<ProjectContext | undefined> {
-    let projectPick = vscode.window.createQuickPick<ProjectContextItem>();
-    projectPick.items = projects.map((value) => {
-        return {
-            context: value,
-            label: value.project.name,
-            description: value.client.name
-        } as ProjectContextItem;
-    });
-    projectPick.show();
-
-    return new Promise<ProjectContext | undefined>((resolve) => {
-        let activeItem: ProjectContextItem;
-        projectPick.onDidChangeSelection((e) => {
-            const result = projectPick.activeItems[0];
-            if (result) {
-                resolve(result.context);
-                projectPick.hide();
-            }
-        });
-        projectPick.onDidChangeValue(value => {
-            if (activeItem && !value && (projectPick.activeItems.length !== 1 || projectPick.activeItems[0] !== activeItem)) {
-                projectPick.activeItems = [activeItem];
-            }
-        });
-        projectPick.onDidAccept((e) => {
-            const result = projectPick.activeItems[0];
-            if (result) {
-                resolve(result.context);
-                projectPick.hide();
-            }
-        });
-        projectPick.onDidHide((e) => {
-            resolve(undefined);
-            projectPick.dispose();
-        });
-    });
+  let result = await pick(
+    items,
+    items.find((item) => item.context === activeItem)
+  );
+  if (result) {
+    return result.context;
+  }
+  return undefined;
 }
 
 interface TargetItem extends vscode.QuickPickItem {
-    target: model.Target;
+  target: model.Target;
 }
 
-async function pickTarget(context: ProjectContext): Promise<model.Target | undefined> {
-    let targetPick = vscode.window.createQuickPick<TargetItem>();
-    targetPick.items = context.client.projectTargets.map((value) => {
-        return {
-            target: value,
-            label: value.name,
-            description: value.type
-        };
-    });
-    targetPick.show();
+async function pickTarget(
+  context: ProjectContext
+): Promise<model.Target | undefined> {
+  let items = context.client.projectTargets.map((value) => {
+    return {
+      target: value,
+      label: value.name,
+      description: value.type
+    } as TargetItem;
+  });
 
-    return new Promise<model.Target | undefined>((resolve) => {
-        let activeItem: TargetItem;
-        targetPick.onDidChangeSelection((e) => {
-            const result = targetPick.activeItems[0];
-            if (result) {
-                resolve(result.target);
-                targetPick.hide();
-            }
-        });
-        targetPick.onDidChangeValue(value => {
-            if (activeItem && !value && (targetPick.activeItems.length !== 1 || targetPick.activeItems[0] !== activeItem)) {
-                targetPick.activeItems = [activeItem];
-            }
-        });
-        targetPick.onDidAccept((e) => {
-            const result = targetPick.activeItems[0];
-            if (result) {
-                resolve(result.target);
-                targetPick.hide();
-            }
-        });
-        targetPick.onDidHide((e) => {
-            resolve(undefined);
-            targetPick.dispose();
-        });
-    });
+  let result = await pick(
+    items,
+    items.find((item) => item.target === context.client.target)
+  );
+  if (result) {
+    return result.target;
+  }
+  return undefined;
 }
 
 interface CMakeConfigurationItem extends vscode.QuickPickItem {
-    config : CMakeConfiguration;
-    edit?: boolean;
-}
-async function pickConfiguration(context: ProjectContext): Promise<CMakeConfiguration | undefined> {
-    let configPick = vscode.window.createQuickPick<CMakeConfigurationItem>();
-    let items = context.client.configurations.map((value) => {
-        return {
-            config: value,
-            label: value.name,
-            description: value.description
-        } as CMakeConfigurationItem;
-    });
-    items.push({ config: {} as CMakeConfiguration, label: "Edit Configurations...", edit: true});
-    configPick.items = items;
-    configPick.show();
-
-    return new Promise<CMakeConfiguration | undefined>((resolve) => {
-        let activeItem: CMakeConfigurationItem;
-        configPick.onDidChangeSelection((e) => {
-            const result = configPick.activeItems[0];
-            if (result) {
-                if (!result.edit) {
-                    resolve(result.config);
-                } else {
-                    vscode.commands.executeCommand("cmake.editCurrentConfigurations").then(() => resolve(undefined));
-                }
-                configPick.hide();
-            }
-        });
-        configPick.onDidChangeValue(value => {
-            if (activeItem && !value && (configPick.activeItems.length !== 1 || configPick.activeItems[0] !== activeItem)) {
-                configPick.activeItems = [activeItem];
-            }
-        });
-        configPick.onDidAccept((e) => {
-            const result = configPick.activeItems[0];
-            if (result) {
-                if (!result.edit) {
-                    resolve(result.config);
-                } else {
-                    vscode.commands.executeCommand("cmake.editCurrentConfigurations").then(() => resolve(undefined));
-                }
-                configPick.hide();
-            }
-        });
-        configPick.onDidHide((e) => {
-            resolve(undefined);
-            configPick.dispose();
-        });
-    });
+  config: CMakeConfiguration;
+  edit?: boolean;
 }
 
-export { ProjectContext, pickProject, pickTarget, pickClient, pickConfiguration };
+async function pickConfiguration(
+  context: ProjectContext
+): Promise<CMakeConfiguration | undefined> {
+  let items = context.client.configurations.map((value) => {
+    return {
+      config: value,
+      label: value.name,
+      description: value.description
+    } as CMakeConfigurationItem;
+  });
+  items.push({
+    config: {} as CMakeConfiguration,
+    label: "Edit Configurations...",
+    edit: true
+  });
+
+  let result = await pick(
+    items,
+    items.find((item) => item.config === context.client.configuration)
+  );
+  if (result) {
+    if (result.edit) {
+      await vscode.commands.executeCommand("cmake.editCurrentConfigurations");
+      return undefined;
+    }
+    return result.config;
+  }
+  return undefined;
+}
+
+export {
+  ProjectContext,
+  pickProject,
+  pickTarget,
+  pickClient,
+  pickConfiguration
+};
