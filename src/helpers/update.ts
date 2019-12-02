482d8ddffe9efd3f391c8dd1b8787dc630ca6829
Christoph Seitz <christoph.seitz@posteo.de>
2019-12-02 21:12:46 +0100

    Add update helper for 0.7.0.

--- src/helpers/update.ts
+++ src/helpers/update.ts
@@ -28,23 +28,47 @@ async function checkForUpdate(version: string, oldVersion: string) {
 
   // Dev updates
   if (oldMajor === "0") {
-    if (oldMinor === "3") {
-      const actions = [
-        "Edit Configurations",
-        "Documentation",
-        "Changelog"
-      ];
+    if (oldMinor <= "3") {
+      const actions = ["Edit Configurations", "Documentation", "Changelog"];
       let result = await window.showInformationMessage(
-        `CMake Integration for VS Code has been updated to ${version}. ` + 
-        `The configuration through settings has changed to a custom ` +
-        `configuration file called "cmake_configurations.json"`,
+        `CMake Integration for VS Code has been updated to ${version}. ` +
+          `The configuration through settings has changed to a custom ` +
+          `configuration file called "cmake_configurations.json"`,
         ...actions
       );
       if (result === actions[0]) {
         commands.executeCommand("cmake.editConfiguration");
       }
       if (result === actions[1]) {
-        env.openExternal(Uri.parse("https://go2sh.github.io/cmake-integration-vscode/"));
+        env.openExternal(
+          Uri.parse("https://go2sh.github.io/cmake-integration-vscode/")
+        );
+      }
+      if (result === actions[2]) {
+        env.openExternal(
+          Uri.parse(
+            "https://github.com/go2sh/cmake-integration-vscode/blob/master/CHANGELOG.md"
+          )
+        );
+      }
+    }
+    if (oldMinor <= "6") {
+      const actions = ["Edit Settings", "Documentation", "Changelog"];
+      let result = await window.showInformationMessage(
+        `CMake Integration for VS Code has been updated to ${version}. ` +
+          `Some settings keys have been placed into new subkeys. ` +
+          `Please check your settings files for any mismatches.`,
+        ...actions
+      );
+      if (result === actions[0]) {
+        commands.executeCommand("workbench.action.openSettings");
+      }
+      if (result === actions[1]) {
+        env.openExternal(
+          Uri.parse(
+            "https://go2sh.github.io/cmake-integration-vscode/reference/settings.html#configuration-defaults"
+          )
+        );
       }
       if (result === actions[2]) {
         env.openExternal(
@@ -57,4 +81,25 @@ async function checkForUpdate(version: string, oldVersion: string) {
   }
 }
 
-export { checkForUpdate };
+async function firstStart(version: string) {
+  const actions = ["Edit Settings", "Edit Configurations", "Documentation"];
+  let result = await window.showInformationMessage(
+    `Welcome to CMake Integration for VS Code version ${version}. ` +
+      `Set your desired build environment in the settings, ` +
+      `edit your build configurations and you are ready to go!`,
+    ...actions
+  );
+  if (result === actions[0]) {
+    commands.executeCommand("workbench.action.openSettings");
+  }
+  if (result === actions[1]) {
+    commands.executeCommand("cmake.editConfiguration");
+  }
+  if (result === actions[2]) {
+    env.openExternal(
+      Uri.parse("https://go2sh.github.io/cmake-integration-vscode/")
+    );
+  }
+}
+
+export { checkForUpdate, firstStart };
